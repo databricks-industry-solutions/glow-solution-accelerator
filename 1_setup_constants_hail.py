2e7ce2b2add1ddbca63763e780187ef99c09674b
dbbnicole <dbbnicole>
2022-10-24 02:55:22 +0000

    Add repo and web reference

--- 1_setup_constants_hail.py
+++ 1_setup_constants_hail.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+# MAGIC %md 
+# MAGIC You may find this series of notebooks at https://github.com/databricks-industry-solutions/glow-solution-accelerator. For more information about this solution accelerator, visit https://www.databricks.com/solutions/accelerators/genome-wide-association-studies.
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC 
 # MAGIC #### Set up constants for Hail to use in GWAS workflow notebooks
